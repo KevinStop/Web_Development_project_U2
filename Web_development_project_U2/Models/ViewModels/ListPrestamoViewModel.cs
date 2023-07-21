659020073de2b7627589c2efd015424912a62762
AlexSG <84851263+SteveenStopper@users.noreply.github.com>
2023-07-21 01:00:32 -0500

    Lista View Model de prestamos
    Go go go

--- Web_development_project_U2/Models/ViewModels/ListPrestamoViewModel.cs
+++ Web_development_project_U2/Models/ViewModels/ListPrestamoViewModel.cs
@@ -7,5 +7,13 @@ namespace Web_development_project_U2.Models.ViewModels
 {
     public class ListPrestamoViewModel
     {
+        public int Id { get; set; }
+        public int UsuarioId { get; set; }
+        public int LibroId { get; set; }
+        public DateTime FechaPrestamo { get; set; }
+        public DateTime FechaDevolucion { get; set; }
+        public virtual ListUsuarioViewModel Usuario { get; set; }
+        public string NombreUsuario { get; set; }
+        public string TituloLibro { get; set; }
     }
 }
